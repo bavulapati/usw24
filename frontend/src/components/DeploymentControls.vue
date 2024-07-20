0c6edf032800a92f18a7bc2af38cd1087f6142d8
Radu Imbuzan <raduimbuzan@gmail.com>
2024-07-20 12:46:35 +0300

    Create page to fetch the markdown file and see the preview

---
+++ frontend/src/components/DeploymentControls.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="h-full">
+    <div class="ms-5 flex w-[30%] items-end">
+      <button type="button"
+        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Deploy</button>
+      <button type="button"
+        class="focus:outline-none text-white bg-yellow-500 hover:bg-yellow-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Redeploy</button>
+      <button type="button"
+        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/PreviewPage.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps(['renderContent'])
+</script>
+
+<template>
+  <main class="h-full">
+    <div id="page-preview-container" class="w-full h-full overflow-scroll p-2.5 rounded-lg border border-gray-300"
+      v-html="renderContent">
+    </div>
+  </main>
+</template>
+
+<style scoped>
+:deep(img),
+:deep(svg),
+:deep(video),
+:deep(canvas),
+:deep(audio),
+:deep(iframe),
+:deep(embed),
+:deep(object) {
+  display: initial;
+}
+</style>
